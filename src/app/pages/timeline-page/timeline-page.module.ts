5d3f2accb9584ec2f0212e28038e140c45465327
ansskf <anss.khaled00@gmail.com>
2020-10-16 02:31:18 +0200

    Added required timeline-page sub-components

--- src/app/pages/timeline-page/timeline-page.module.ts
+++ src/app/pages/timeline-page/timeline-page.module.ts
@@ -3,16 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { TimelinePageComponent } from './timeline-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 const ROUTES: Routes = [
   {
     path: '',
     component: TimelinePageComponent,
+    children: [
+      {
+        path: '',
+        component: PostsListComponent
+      },
+      {
+        path: ':id',
+        component: PostDetailsComponent
+      }
+    ]
   }
 ]
 
 @NgModule({
-  declarations: [TimelinePageComponent],
+  declarations: [TimelinePageComponent, PostsListComponent, PostDetailsComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(ROUTES)

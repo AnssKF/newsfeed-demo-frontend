fe604108afe21879615541d765b5b54aa2cbebcd
ansskf <anss.khaled00@gmail.com>
2020-10-16 02:22:40 +0200

    Add Autherization Bearer token to header if user is logged in

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -7,19 +7,29 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
 
   baseUrl = environment.baseUrl
+  loginStatus = this.authService.loginStatusChange.value
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     
     if(request.url.indexOf('api') !== -1){
       request = request.clone({
         url: `${this.baseUrl}${request.url}`
+      })
+    }
+
+    if(this.loginStatus.isLoggedIn){
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.loginStatus.accessToken}`
+        }
       })
     }
 
